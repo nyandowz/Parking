ccde48f21581bf6572ad09531f378bf4d23ce1e6
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2023-04-02 15:04:40 +0530

    finela done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_background/flutter_background.dart';
 import 'package:get/get.dart';
 import 'package:smart_car_parking/controller/splace_controller.dart';
 import 'package:smart_car_parking/pages/homepage/homepage.dart';

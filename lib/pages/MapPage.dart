e807759922330274d3586c25428aa3172ff4e395
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2023-10-29 00:04:11 +0530

    completed

--- lib/pages/MapPage.dart
+++ lib/pages/MapPage.dart
@@ -14,37 +14,10 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  BitmapDescriptor parkingSlot1 = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure);
-  BitmapDescriptor parkingSlot2 = BitmapDescriptor.defaultMarker;
-  BitmapDescriptor parkingSlot3 = BitmapDescriptor.defaultMarker;
-  BitmapDescriptor parkingSlot4 = BitmapDescriptor.defaultMarker;
 
-  void allMarkers() {
-    BitmapDescriptor.fromAssetImage(
-            ImageConfiguration.empty, "assets/images/parking1.jpg")
-        .then((Icon) {
-      parkingSlot1 = Icon;
-    });
-    BitmapDescriptor.fromAssetImage(
-            ImageConfiguration.empty, "assets/images/parking2.jpg")
-        .then((Icon) {
-      parkingSlot2 = Icon;
-    });
-    BitmapDescriptor.fromAssetImage(
-            ImageConfiguration.empty, "assets/images/parking3.jpg")
-        .then((Icon) {
-      parkingSlot3 = Icon;
-    });
-    BitmapDescriptor.fromAssetImage(
-            ImageConfiguration.empty, "assets/images/parking4.jpeg")
-        .then((Icon) {
-      parkingSlot4 = Icon;
-    });
-  }
 
   @override
   void initState() {
-    allMarkers();
     super.initState();
   }
 
@@ -54,39 +27,39 @@ class _MapPageState extends State<MapPage> {
     const LatLng center = LatLng(23.25226182392082, 77.48536382627971);
 
     return Scaffold(
-       appBar: AppBar(
-          backgroundColor: blueColor,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/images/white_logo.png",
-                width: 40,
-                height: 40,
+      appBar: AppBar(
+        backgroundColor: blueColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/white_logo.png",
+              width: 40,
+              height: 40,
+            ),
+            const SizedBox(width: 20),
+            const Text(
+              "SMART CAR PARKING",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
               ),
-              const SizedBox(width: 20),
-              const Text(
-                "SMART CAR PARKING",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  // parkingController.timeCounter();
-                  Get.toNamed("/about-us");
-                },
-                icon: const Icon(
-                  Icons.person,
-                  color: Colors.white,
-                )),
+            ),
           ],
-          centerTitle: true,
         ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                // parkingController.timeCounter();
+                Get.toNamed("/about-us");
+              },
+              icon: const Icon(
+                Icons.person,
+                color: Colors.white,
+              )),
+        ],
+        centerTitle: true,
+      ),
       body: GoogleMap(
         buildingsEnabled: true,
         compassEnabled: true,
@@ -106,25 +79,33 @@ class _MapPageState extends State<MapPage> {
           ),
           Marker(
             visible: true,
-            onTap: () {    Get.to(HomePage());},
+            onTap: () {
+              Get.to(HomePage());
+            },
             markerId: MarkerId('parking_2'),
             position: LatLng(23.252025246281843, 77.48283182115601),
           ),
           Marker(
             visible: true,
-            onTap: () {    Get.to(HomePage());},
+            onTap: () {
+              Get.to(HomePage());
+            },
             markerId: MarkerId('parking_3'),
             position: LatLng(23.251670379036387, 77.47907672881156),
           ),
           Marker(
             visible: true,
-            onTap: () {    Get.to(HomePage());},
+            onTap: () {
+              Get.to(HomePage());
+            },
             markerId: MarkerId('parking_4'),
             position: LatLng(23.2484962466233, 77.48594318366699),
           ),
           Marker(
             visible: true,
-            onTap: () {    Get.to(HomePage());},
+            onTap: () {
+              Get.to(HomePage());
+            },
             markerId: MarkerId('parking_5'),
             position: LatLng(23.251951095267497, 77.48558382672624),
           ),
